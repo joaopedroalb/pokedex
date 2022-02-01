124104d2af1183088509c56299ffc18b757f8643
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-01 11:14:15 -0300

    Add enum types pokemon

---
+++ src/Data/Enum.ts
@@ -0,0 +1,3 @@
+export type PokemonType = "bug" | "dark" | "dragon" | "eletric" | "fairy" | "fighting" | "fire" | "flying" |
+                         "ghost" | "grass " | "ground" | "ice" | "normal" | "poison" | "psychic" | "rock" |
+                          "steel" | "water"
