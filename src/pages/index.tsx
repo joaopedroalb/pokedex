c26fa79f114c1607f951401f0c6b7d2a32ec8906
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-04 13:17:53 -0300

    Add fetch in scroll down

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,32 @@ type Pokemon = {
 const Home: NextPage = () => {
 
   const [lstPokemons, setLstPokemons] = useState<Array<Pokemon>|null>(null)
+  const [loading,setLoading] = useState(false)
+
+  const [limit,setLimit] = useState(0)
+
+  
+
+  async function handleScroll(){
+    if(window.innerHeight + document.documentElement.scrollTop + 2 < document.documentElement.offsetHeight || loading || limit > 600)
+      return
+    
+    setLimit((value)=>value+36)
+  }
 
   useEffect(()=>{
     async function handleFetch(){
-      const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=36&offset=0')
+      const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`)
       const data = await resp.json()
-      console.log(data)
       setLstPokemons(data.results)
     }
-
     handleFetch()
+  },[limit])
+
+
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+    return () => window.removeEventListener('scroll',handleScroll)
   },[])
   
 
