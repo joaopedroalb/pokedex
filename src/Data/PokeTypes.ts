0a65a3d116eaff9cedd86c264aa44edb179a2e1f
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-02-07 18:41:37 -0300

    Create pokeType file

---
+++ src/Data/PokeTypes.ts
@@ -0,0 +1,101 @@
+type PokeType = {
+    name:string
+    relations:TypeRelation[]
+}   
+
+type TypeRelation = {
+    pokeType:PokeType
+    rate:number
+}
+
+export const bugType:PokeType = {
+    name:'bug',
+    relations:[]
+}
+
+export const darkType:PokeType = {
+    name:'dark',
+    relations:[]
+}
+
+export const dragonType:PokeType = {
+    name:'dragon',
+    relations:[]
+}
+
+export const electricType:PokeType = {
+    name:'electric',
+    relations:[]
+}
+
+export const fairyType:PokeType = {
+    name:'fairy',
+    relations:[]
+}
+
+export const fightingType:PokeType = {
+    name:'fighting',
+    relations:[]
+}
+
+export const fireType:PokeType = {
+    name:'fire',
+    relations:[]
+}
+
+export const flyingType:PokeType = {
+    name:'flying',
+    relations:[]
+}
+
+export const ghostType:PokeType = {
+    name:'ghost',
+    relations:[]
+}
+
+export const grassType:PokeType = {
+    name:'grass',
+    relations:[]
+}
+
+export const groundType:PokeType = {
+    name:'ground',
+    relations:[]
+}
+
+export const iceType:PokeType = {
+    name:'ice',
+    relations:[]
+}
+
+export const normalType:PokeType = {
+    name:'normal',
+    relations:[]
+}
+
+export const poisonType:PokeType = {
+    name:'poison',
+    relations:[]
+}
+
+export const psychicType:PokeType = {
+    name:'psychic',
+    relations:[]
+}
+
+export const rockType:PokeType = {
+    name:'rock',
+    relations:[]
+}
+
+export const steelType:PokeType = {
+    name:'steel',
+    relations:[]
+}
+
+export const waterType:PokeType = {
+    name:'water',
+    relations:[]
+}
+
+ 
